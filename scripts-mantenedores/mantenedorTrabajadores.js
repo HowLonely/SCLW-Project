e9512b5b5e46ced48a5f10280d3d044e552227c0
nicolas-000 <nicolas.ayala.a@protonmail.ch>
2023-05-02 23:29:53 -0400

    crud mantenedor de trabajadores

---
+++ scripts-mantenedores/mantenedorTrabajadores.js
@@ -0,0 +1,28 @@
+function sacarId(id) {
+    if (Array.isArray(id)) {
+        ids = id.map(x => document.getElementById(x));
+        return ids;
+    } else {
+        ids = document.getElementById(id);
+        return ids;
+    }
+}
+
+function verificarParametrosTrabajador() {
+    ids = sacarId(['nombre', 'apellido', 'rut', 'telefono', 'cargo', 'faena-actual', 'fotografia']);
+    values = ids.map(x => x.value)
+
+    alertFormTrabajadores = document.getElementById('alert-form-trabajadores');
+    alertFormTrabajadores.innerHTML = ""
+
+    if (values.every(x => x != "")) {
+        alert("Se ha ingresado correctamente");
+    } else {
+        vacios = ids.filter(x => x.value == "").map(x => x.id);
+        for (x of vacios) {
+            parrafo = '<p>Debe ingresar ' + x + '</p>';
+            alertFormTrabajadores.insertAdjacentHTML('beforeend', parrafo);
+        }
+    }
+    
+}
