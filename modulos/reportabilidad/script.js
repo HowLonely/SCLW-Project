64a341cc98117e6a2dd8883709316cfef2ca2830
HowLonely <ellian.andre1203@gmail.com>
2023-05-04 12:30:26 -0400

    Añadida otra opcion dentro del select-reporte

--- modulos/reportabilidad/script.js
+++ modulos/reportabilidad/script.js
@@ -1,35 +1,42 @@
-function ocultarSelect() {
-    select1 = document.getElementById('select-fecha');
-    select2 = document.getElementById('select-faena');
-    select3 = document.getElementById('select-trabajador');
+ocultarSelect(); //Funcion limpieza
 
-    select1.style.display = 'none';
-    select2.style.display = 'none';
-    select3.style.display = 'none';
+function ocultarSelect() {
+  select1 = document.getElementById("select-historico");
+  select2 = document.getElementById("select-fecha");
+  select3 = document.getElementById("select-faena");
+  select4 = document.getElementById("select-trabajador");
+
+  select1.style.display = "none";
+  select2.style.display = "none";
+  select3.style.display = "none";
+  select4.style.display = "none";
 }
 
 function cambiardisplay(id) {
-    res = document.getElementById(id);
-    if (res.style.display == 'none') {
-        res.style.display = 'flex';
-    } else {
-        res.style.displat = 'none';
-    }
+  res = document.getElementById(id);
+  if (res.style.display == "none") {
+    res.style.display = "flex";
+  } else {
+    res.style.displat = "none";
+  }
 }
 
 function mostrarSelect() {
-    reporte = document.getElementById('select-reporte');
-    valueReporte = reporte.value;
-    ocultarSelect();
-    switch (valueReporte) {
-        case '1':
-            cambiardisplay('select-fecha');
-            break;
-        case '2':
-            cambiardisplay('select-faena');
-            break;
-        case '3':
-            cambiardisplay('select-trabajador');
-            break;
-    }
+  reporte = document.getElementById("select-reporte");
+  valueReporte = reporte.value;
+  ocultarSelect();
+  switch (valueReporte) {
+    case "1":
+      cambiardisplay("select-historico");
+      break;
+    case "2":
+      cambiardisplay("select-fecha");
+      break;
+    case "3":
+      cambiardisplay("select-faena");
+      break;
+    case "4":
+      cambiardisplay("select-trabajador");
+      break;
+  }
 }
