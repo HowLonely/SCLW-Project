37089f92ee5d0dfdfa167fe592297b330d9c6b4c
nicolas-000 <nicolas.ayala.a@protonmail.ch>
2023-05-04 11:52:23 -0400

    cambiando a onchange y agregando cambios en fecha de modulo APR

--- modulos/solicitudes-apr/script.js
+++ modulos/solicitudes-apr/script.js
@@ -1,21 +1,29 @@
-function aprobado(id, idAprobar, idRechazar) {
+function aprobado(id, idFecha , idAprobar, idRechazar) {
     res = confirm("¿Desea aprobar esta solicitud?");
     if (res) {
         respuesta = document.getElementById(id);
         respuesta.innerHTML = '<span class="badge rounded-pill text-bg-success">Aprobado</span>';
         alert("Se ha aprobado la solicitud");
+        cambiarColorFecha(idFecha);
         deshabilitarBotones(idAprobar, idRechazar);
     }
 }
 
-function rechazado(id, idAprobar, idRechazar) {
+function rechazado(id, idFecha, idAprobar, idRechazar) {
     res = confirm("¿Desea rechazar esta solicitud?");
     if (res) {
         res = document.getElementById(id);
         res.innerHTML = '<span class="badge rounded-pill text-bg-danger">Rechazado</span>';
         alert("Se ha rechazado la solicitud");
+        cambiarColorFecha(idFecha);
         deshabilitarBotones(idAprobar, idRechazar);
     }
+}
+
+function cambiarColorFecha(id) {
+    fecha = document.getElementById(id);
+    fecha.classList.remove('text-bg-primary');
+    fecha.classList.add('text-bg-secondary');
 }
 
 function deshabilitarBotones(aprobar, rechazar) {
@@ -26,25 +34,25 @@ function deshabilitarBotones(aprobar, rechazar) {
 }
 
 function aprobado1() {
-    aprobado('respuesta1', 'aprobar1', 'rechazar1');
+    aprobado('respuesta1', 'fecha1', 'aprobar1', 'rechazar1');
 }
 
 function rechazado1() {
-    rechazado('respuesta1', 'aprobar1', 'rechazar1');
+    rechazado('respuesta1', 'fecha1', 'aprobar1', 'rechazar1');
 }
 
 function aprobado2() {
-    aprobado('respuesta2', 'aprobar2', 'rechazar2');
+    aprobado('respuesta2', 'fecha2', 'aprobar2', 'rechazar2');
 }
 
 function rechazado2() {
-    rechazado('respuesta2', 'aprobar2', 'rechazar2');
+    rechazado('respuesta2', 'fecha2', 'aprobar2', 'rechazar2');
 }
 
 function aprobado3() {
-    aprobado('respuesta3', 'aprobar3', 'rechazar3');
+    aprobado('respuesta3', 'fecha3', 'aprobar3', 'rechazar3');
 }
 
 function rechazado3() {
-    rechazado('respuesta3', 'aprobar3', 'rechazar3');
+    rechazado('respuesta3', 'fecha3', 'aprobar3', 'rechazar3');
 }
